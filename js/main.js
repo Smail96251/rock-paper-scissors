e99ed938318333f28ee20453440acf6de263c3b0
Nikita(Smail96251) <hveshchenko@gmail.com>
2023-04-04 20:42:36 +0300

    feat: add functions start game, update player and computer sign, validation game and validation raund game

--- js/main.js
+++ js/main.js
@@ -8,24 +8,86 @@ const buttonGame = document.querySelectorAll(".btn-square");
 const modal = document.getElementById("endgame-modal");
 const overlay = document.getElementById("overlay");
 
+const gameSigns = {
+    "rock": "✊",
+    "paper": "✋",
+    "scissors": "✌"
+}
 let computerChoice = null;
 let playerChoice = null;
+let playerCounter = 0;
+let computerCounter = 0;
 
-function getRandomNuber(){
+function getRandomNumber(){
     return Math.floor(Math.random() * 3);
 }
 
-function getComputerChoise (){
-    computerChoice = getRandomNuber();
+function getComputerChoice (){
+    switch (getRandomNumber()){
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper"
+            break;
+        case 2:
+            computerChoice = "scissors"
+            break;
+    }
 }
 
 function getPlayerChoice(e){
     playerChoice = e.currentTarget.attributes.name.value;
+    startGame();
+}
+
+function updatePlayerAndComputerSign (){
+    playerSign.innerText = gameSigns[playerChoice];
+    computerSign.innerText = gameSigns[computerChoice];
+}
+
+function validateGames() {
+    if(playerChoice == "rock" && computerChoice == "scissors"
+        || playerChoice == "paper" && computerChoice == "rock"
+        || playerChoice == "scissors" && computerChoice == "paper"){
+        playerCounter++;
+        playerScore.innerText = playerCounter;
+        scoreInfo.innerText = "You Won!"
+        scoreMessage.innerText = `${playerChoice} beats ${computerChoice}`
+    }else if(computerChoice == "rock" && playerChoice == "scissors"
+        || computerChoice == "paper" && playerChoice == "rock"
+        || computerChoice == "scissors" && playerChoice == "paper"){
+        computerCounter++;
+        computerScore.innerText = computerCounter;
+        scoreInfo.innerText = "You Lose..."
+        scoreMessage.innerText = `${playerChoice} loses ${computerChoice}`
+    }else {
+        scoreInfo.innerText = "It's a tie!"
+        scoreMessage.innerText = `${playerChoice} ties with ${computerChoice}`
+    }
+}
+
+function validateRoundGame() {
+    if(playerCounter == 5 || computerCounter == 5){
+        playerCounter = 0;
+        computerCounter = 0;
+        computerScore.innerText = computerCounter;
+        playerScore.innerText = playerCounter;
+    }
+}
+
+function startGame (){
+    getComputerChoice();
+    updatePlayerAndComputerSign();
+    validateGames();
+    validateRoundGame();
 }
 
 buttonGame.forEach(item => {
     item.addEventListener('click', getPlayerChoice);
 })
+
+
 
 
 
